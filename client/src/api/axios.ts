22b5fbd4a40e5ce7f77c98e1731046024df144f9
Bernardo <bernardo.soares30@outlook.com>
2026-04-20 09:05:09 -0300

    fix: small fixes in the template

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,9 +1,6 @@
 import axios from 'axios'
-import { handleAuthError } from './interceptors/auth-interceptor'
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 })
-
-api.interceptors.response.use((response) => response, handleAuthError)
